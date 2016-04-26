c40f06957cde6d3bac351b6cb37bf269a977e93c
Yonathan Ayenew <yonayen@gmail.com>
2016-04-25 18:14:02 -0700

    app.js structure and html divs

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// var options = ["question 1","question 2","question 3"]
+
+//Leave this for later
+$(".question-options").on("click","li",function()
+{
+	//Check the answer, then update the quesiton title and options to the next question.
+});
+
+function checkAnswer(index)
+{
+	//Leave this for later.
+}
+
+function updateQuestionTitle(title)
+{
+	//Replace the Title Text
+}
+
+function updateQuestionOptions(options)
+{
+	//1.Empty the UL
+
+	//2.Loop through the options array and append them to the UL
+}
