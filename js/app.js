f1633d8de750c94c291419a4346a802e30aad12a
Yonathan Ayenew <yonayen@gmail.com>
2016-04-29 08:39:18 -0700

    rewardStar function

--- js/app.js
+++ js/app.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
     $(".question-options").on("click", ".answer", function () {
     	currentQuestion++;
     	nextQuestion();
-    	// --- Add star here empty or full --- //
+    	rewardStar(); // This is how stars are awarded Empty = False; Full = True
     });
 
 
@@ -93,77 +93,12 @@ $(document).ready(function() {
     	$(".question-wrapper").html(newQuestion); // displays new question
     });
 
-   //  $(".question-options").on("click", "#submit", function () {
-   //      updateCup();
-   //      currentQuestion++;
-   //      nextQuestion();
-   //  });
 
-   //  $(".question-options").on("click", "#retry_button", function () {
-   //      numberCorrect = 0;
-   //      currentQuestion = 0;
-   //      $(".score_cup").css("display", "none");
-   //      $("#score_cup0").css("display", "inline");
-   //      var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><input type="radio" name="option" class="option" value="0"><span class="answer">'+questions[currentQuestion].choices[0]+'</span><br><input type="radio" name="option" class="option" value="1"><span class="answer">'+questions[currentQuestion].choices[1]+'</span><br><input type="radio" name="option" class="option" value="2"><span class="answer">'+questions[currentQuestion].choices[2]+'</span><br><input type="radio" name="option" class="option" value="3"><span class="answer">'+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="Submit Answer"><span id="hint"></span><input type="button" id="retry_button" value="Try Again!"></div>';
-   //      $("#question_wrapper").html(newQuestion);
-   //      $("#last_question_fact").html("");
-   //  });
+    function rewardStar() {
+    	var answer = $("").val();
+    }
 
-   //  function updateCup() {
-   //      var answer = $("input[type='radio']:checked").val();
-   //      if (answer == questions[currentQuestion].correct) {
-   //          numberCorrect++;
-   //      }
-   //      if (numberCorrect == 1) {
-   //          $(".score_cup").css("display", "none")
-   //          $("#score_cup1").fadeIn();
-   //      }
-   //      else if (numberCorrect == 2) {
-   //          $(".score_cup").css("display", "none")
-   //          $("#score_cup2").fadeIn();
-   //      }
-   //      else if (numberCorrect == 3) {
-   //          $(".score_cup").css("display", "none")
-   //          $("#score_cup3").fadeIn();
-   //      }
-   //      else if (numberCorrect == 4) {
-   //          $(".score_cup").css("display", "none")
-   //          $("#score_cup4").fadeIn();
-   //      }
-   //      else if (numberCorrect == 5) {
-   //          $(".score_cup").css("display", "none")
-   //          $("#score_cup5").fadeIn();
-   //      }
-   //  }
 
-   //  function nextQuestion() {
-   //      if (currentQuestion < 5) {
-   //          $(".question").remove();
-   //          $("#answer_holder input").remove();
-   //          $("#answer_holder span").remove();
-			// $("#last_question_fact").hide();
-   //          var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span><br><div id="answer_holder"><input type="radio" name="option" class="option" value="0"><span class="answer">'+questions[currentQuestion].choices[0]+'</span><br><input type="radio" name="option" class="option" value="1"><span class="answer">'+questions[currentQuestion].choices[1]+'</span><br><input type="radio" name="option" class="option" value="2"><span class="answer">'+questions[currentQuestion].choices[2]+'</span><br><input type="radio" name="option" class="option" value="3"><span class="answer">'+questions[currentQuestion].choices[3]+'</span><br></div><div id="button_holder"><input type="button" id="submit" value="Submit Answer"><span id="hint"></span><input type="button" id="retry_button" value="Try Again!"></div>';
-   //          $("#question_wrapper").html(newQuestion);
-   //          var lastFact= questions[currentQuestion-1].fact;
-   //          $("#last_question_fact").html(lastFact).fadeIn();
-   //      }
-   //      else {
-   //          $(".question").remove();
-   //          $("#answer_holder input").remove();
-   //          $("#answer_holder span").remove();
-			// $("#last_question_fact").fadeOut();
-   //          $("#submit").css("display", "none");
-   //          $("#retry_button").css("display", "inline");
-   //          var lastFact= questions[currentQuestion-1].fact;
-   //          $("#last_question_fact").html(lastFact);
-   //          if (numberCorrect == 1) {
-   //              var finalScore = '<span id="final">Congratulations on finishing the quiz!  You correctly answered '+numberCorrect+' question.'
-   //              $("#answer_holder").html(finalScore);
-   //          }
-   //          else {
-   //              var finalScore = '<span id="final">Congratulations on finishing the quiz!  You correctly answered '+numberCorrect+' questions.'
-   //              $("#answer_holder").html(finalScore);
-   //          }
-   //      }
-   //  }
+
+
 });
